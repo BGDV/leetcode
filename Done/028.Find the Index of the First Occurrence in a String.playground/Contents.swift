bd0e238f28d2676c93f256dbb51bb26c222b8bdf
BGDV <andrey.al.bogdanov@gmail.com>
2025-02-06 00:29:46 +0400

    28 done

---
+++ Done/028.Find the Index of the First Occurrence in a String.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+class Solution {
+    func strStr(_ haystack: String, _ needle: String) -> Int {
+        var result = -1
+        var index = 0
+        while result == -1 && index < haystack.count {
+            var startIndex = index
+            var haystackOffSet = index
+            var needleOffSet = 0
+            repeat {
+                let i = haystack.index(haystack.startIndex, offsetBy: haystackOffSet)
+                let n = needle.index(needle.startIndex, offsetBy: needleOffSet)
+                if haystack[i] == needle[n] {
+                    guard needleOffSet != needle.count-1 else {
+                        result = startIndex
+                        break
+                    }
+                    haystackOffSet += 1
+                    needleOffSet += 1
+                } else {
+                    break
+                }
+            } while needleOffSet < needle.count && result == -1 && haystackOffSet < haystack.count
+            
+            index += 1
+        }
+        return result
+    }
+}
+
+let haystack = "aaa"
+let needle = "aaaa"
+
+Solution().strStr(haystack, needle)
