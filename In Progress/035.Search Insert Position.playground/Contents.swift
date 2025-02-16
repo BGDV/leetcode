6ea693b57a9ab2e63e514c9e5011368223c8c05a
BGDV <andrey.al.bogdanov@gmail.com>
2025-02-16 20:02:24 +0400

    35 :white_check_mark:

---
+++ In Progress/035.Search Insert Position.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+// MARK: 0 ms
+class Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var result = -1
+        var pointer = 0
+        
+        while pointer < nums.count && nums[pointer] <= target {
+            if nums[pointer] == target {
+                result = pointer
+            }
+            pointer += 1
+        }
+        if result == -1 {
+            result = pointer
+        }
+        return result
+    }
+}
+
+let numbers = [1]
+let target = 1
+
+Solution().searchInsert(numbers, target)
+
+
+
